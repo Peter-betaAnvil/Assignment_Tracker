2171a742c886c25f62376494ca996523b01b911d
feranmi <feranmiosungbure@gmail.com>
2026-03-17 21:46:24 +0100

    day 7, card update

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:assignment_tracker/widget/assignment_card.dart';
+import 'package:assignment_tracker/widgets/assignment_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,25 +12,48 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
       ) ,
 
-      body: ListView(
-        padding: const EdgeInsets.all(12),
-        children: const[
-          AssignmentCard(
-            title: "Linked List Report",
-            course: "CSC 201",
-            dueDate: "March 25",
-            priorityColor: Colors.red,
-          ),
-          AssignmentCard(
-            title: "Integration Assignment",
-            course: "MTH 203",
-            dueDate: "March 27",
-            priorityColor: Colors.orange,
-          )
-        ],
+      body: Container(
+        color: const Color(0xFFF5F7FB),
+        child: Column(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsetsGeometry.all(12),
+                  child: Text("Upcoming Assignments",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ],
+            ),
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.all(12),
+                children: const[
+                  AssignmentCard(
+                    title: "Linked List Report",
+                    course: "CSC 201",
+                    dueDate: "March 25",
+                    priority: "High",
+                  ),
+                  AssignmentCard(
+                    title: "Integration Assignment",
+                    course: "MTH 203",
+                    dueDate: "March 27",
+                    priority: "Medium",
+                  )
+                ],
+              ),
+            ),
+          ],
+        )
       ),
 
-      floatingActionButton: FloatinActionButton(
+      floatingActionButton: FloatingActionButton(
         onPressed: (){},
         child: const Icon(Icons.add),
       )
--- lib/widgets/assignment_card.dart
+++ lib/widgets/assignment_card.dart
@@ -4,26 +4,109 @@ class AssignmentCard extends StatelessWidget{
   final String title;
   final String course;
   final String dueDate;
-  final Color priorityColor;
+  final String priority;
 
   const AssignmentCard({
     super.key,
     required this.title,
     required this.course,
     required this.dueDate,
-    required this.priorityColor,
+    required this.priority,
   });
+
+  Color  getPriorityColor(){
+    switch(priority) {
+      case "High":
+        return Colors.red;
+      case "Medium":
+        return Colors.orange;
+      default:
+        return Colors.green;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      margin: const EdgeInsets.only(bottom: 12),
-      child: ListTile(
-        title: Text(title),
-        subtitle: Text("$course . Due $dueDate"),
-        trailing: Icon(Icons.flag, color: priorityColor),
-      )
+    return Container(
+      margin: const EdgeInsets.only(bottom: 14),
+      padding: const EdgeInsets.all(14),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 6,
+            offset: Offset(0, 3),
+          )
+        ],
+      ),
+      child: Row(
+        children: [
+          //priority indicator bar
+          Container(
+            width: 6,
+            height: 60,
+            decoration: BoxDecoration(
+              color: getPriorityColor(),
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+
+          const SizedBox(width: 12,),
+
+          //Text section
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  course,
+                  style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(height: 4,),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height:6,),
+                Text(
+                  "Due $dueDate",
+                  style: const TextStyle(
+                    fontSize: 13,
+                    color: Colors.black54,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          //priority badge
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 4,
+            ),
+            decoration: BoxDecoration(
+              color: getPriorityColor().withOpacity(0.15),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Text(
+              priority,
+              style: TextStyle(
+                color: getPriorityColor(),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
