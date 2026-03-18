52d3a353c17983c5f522c90ee521051734c7365e
feranmi <feranmiosungbure@gmail.com>
2026-03-18 21:22:34 +0100

    updated screen day 8

---
+++ lib/screens/add_assignment_screen/add_assignment_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddAssignmentScreen extends StatefulWidget {
+  const AddAssignmentScreen({super.key});
+
+  @override
+  State<AddAssignmentScreen> createState() => _AddAssignmentScreenState();
+}
+
+class _AddAssignmentScreenState extends State<AddAssignmentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
