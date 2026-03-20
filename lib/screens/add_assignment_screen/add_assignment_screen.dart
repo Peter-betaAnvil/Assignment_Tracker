8917209659d2b15fa777c29884410b5b18cbbe30
feranmi <feranmiosungbure@gmail.com>
2026-03-20 23:30:45 +0100

    add assignment screen update day 10

--- lib/screens/add_assignment_screen/add_assignment_screen.dart
+++ lib/screens/add_assignment_screen/add_assignment_screen.dart
@@ -10,6 +10,6 @@ class AddAssignmentScreen extends StatefulWidget {
 class _AddAssignmentScreenState extends State<AddAssignmentScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
