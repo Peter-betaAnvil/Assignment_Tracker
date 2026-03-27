620f3dbf525aafe11120043db4e8fefadc271c35
feranmi <feranmiosungbure@gmail.com>
2026-03-27 23:31:15 +0100

    day 17

--- lib/screens/add_assignment_screen/add_assignment_screen.dart
+++ lib/screens/add_assignment_screen/add_assignment_screen.dart
@@ -66,7 +66,7 @@ class _AddAssignmentScreenState extends State<AddAssignmentScreen> {
             ),
 
             const SizedBox(height: 16),
-            const SizedBox(height: 16),
+
             
           ]
         )
